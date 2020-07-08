20596241b5b32ee8372d9cfed7af699df3e28e1b
ksa <sergey@karandin.ru>
2020-07-08 10:26:40 +0300

    controls

--- 40_control/main.go
+++ 40_control/main.go
@@ -56,7 +56,54 @@ func main() {
 		fmt.Println("range slice by index", idx)
 	}
 	for idx, val := range sl {
-		fmt.Println("range slice by index", idx, val)
+		fmt.Println("range slice by idx-value", idx, val)
+	}
+	for _, val := range sl {
+		fmt.Println("range slice by idx-value", val)
 	}
 
+	//операции по map
+	for key := range mm {
+		fmt.Println("range map by key", key)
+	}
+	for key, val := range mm {
+		fmt.Println("range map by key-val", key, val)
+	}
+	for _, val := range mm {
+		fmt.Println("range map by val", val)
+	}
+
+	//операция switch  - потестировать все возможные варианты: есть специфика
+	mm["firstName"] = "Sergey"
+	mm["flag"] = "Ok"
+	switch mm["firstName"] {
+	case "Sergey", "Evgeny":
+		println("switch - name is Sergey")
+		//если кейс выполнился, остальные нижележащие не выполняются.
+	case "Petr":
+		if mm["flag"] == "Ok" {
+			break // принудительный выход из switch
+		}
+		println("switch - name is Petr")
+		fallthrough //переход на следующий вариант
+	default:
+		println("switch - some other name")
+	}
+	//как замена множественным if else
+	switch {
+	case mm["firstName"] == "Sergey":
+		println("Switch2 - Sergey")
+	case mm["lastName"] == "Karandin":
+		println("Switch2 - Karandin")
+	}
+	//выход из цикла будучи внутри switch
+MyLoop: //лэйбл
+	for key, val := range mm {
+		println("switch in loop", key, val)
+		switch {
+		case key == "firstName" && val == "Sergey":
+			println("switch - break loop here")
+			break MyLoop
+		}
+	}
 }
